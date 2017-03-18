372b02279205a3cfc03576d6feb8897b991c19d8
Wojciech Kocik <wojciechkocik@hotmail.com>
2017-03-18 23:01:22 +0100

    fun with rxtx

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import gnu.io.CommPortIdentifier;
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.SerialPort;
+
+import java.util.Date;
+import java.util.Enumeration;
+
+/**
+ * Created by Wojciech on 18.03.2017.
+ */
+public class Main{
+
+    public static void main(String[] args){
+        System.out.print(new Date());
+        Enumeration ports = CommPortIdentifier.getPortIdentifiers();
+
+        while(ports.hasMoreElements()){
+            System.out.println(((CommPortIdentifier)ports.nextElement()).getName());
+        }
+
+        try {
+            CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier("COM5");
+            SerialPort serialPort = (SerialPort) portIdentifier.open("NameOfConnection-whatever", 0);
+            System.out.println("asf");
+        } catch (NoSuchPortException e) {
+            e.printStackTrace();
+        } catch (PortInUseException e) {
+            e.printStackTrace();
+        }
+    }
+}
